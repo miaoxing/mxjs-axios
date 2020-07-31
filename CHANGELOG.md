61f0f08a035c53e99cc3f800c49e1a8a30ae6f81
twinh <twinhuang@qq.com>
2020-07-31 15:08:46 +0800

    refactor: 更新包名和目录，避免前后端冲突

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 0.1.0 (2020-07-30)
+
+
+### Features
+
+* 拆分出 axios 功能 ([](https://github.com/miaoxing/mxjs-axios/commit/))
---
+++ index.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import $ from 'miaoxing';
+
+const NOT_FOUND = 404;
+const UNAUTHORIZED_CODE = 401;
+const TIPS_DELAY = 3000;
+
+function showError(error) {
+  if (error.config && error.config.ignoreError) {
+    return;
+  }
+
+  $.err(error.response.status === NOT_FOUND ? '很抱歉，您访问的页面不存在，请检查后再试' : '很抱歉，请求出错，请稍后再试');
+}
+
+axios.interceptors.request.use(config => {
+  config.loading && $.loading('show');
+  return config;
+});
+
+axios.interceptors.response.use(response => {
+  // 未登录,跳转到登录地址
+  if (typeof response.data.code !== 'undefined' && response.data.code === UNAUTHORIZED_CODE) {
+    setTimeout(function () {
+      window.location.href = response.data.next;
+    }, TIPS_DELAY);
+  }
+
+  $.loading('hide');
+  return response;
+}, error => {
+  $.loading('hide');
+  showError(error);
+  return Promise.reject(error);
+});
+
+export default axios;
